5dd20d996faad77c86d92a330b06d7812960810b
DanielDM123 <94938720+DanielDM123@users.noreply.github.com>
2026-03-08 19:47:11 +0000

    Can read the songs from a specific setlist from the API

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SetlistComponent from './setlistAPI'
+import SetlistApiComponent from './setlistAPI'
 
 // Franz Ferdinand
 
@@ -22,10 +22,12 @@ function App() {
 
   return (
     <>
+      <p>franz ferdinand</p>
+      <p>aa7a2827-f74b-473c-bd79-03d065835cf7</p>
       <input id="artist_input" name="artist_input" type="text" onChange={ getUserInputtedArtist }/>
       <button onClick={ clickButton }>Click Me</button>
 
-      <SetlistComponent artistName={artist} />
+      <SetlistApiComponent artistName={artist} />
       {/* <SetlistComponent artistName="The Beatles" /> */}
     </>
   )
--- src/setlistAPI.jsx
+++ src/setlistAPI.jsx
@@ -2,12 +2,22 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 // const setlistURL = '/api/1.0/artist/b10bbbfc-cf9e-42e0-be17-e2c3e1d2600d';
-const setlistKey = '1c1QlARHxXsUKzGmRT1Tnp3YoDZwTt4R22To';
+const setlistApiKey = '1c1QlARHxXsUKzGmRT1Tnp3YoDZwTt4R22To';
 
-const SetlistComponent = ({ artistName }) => {
+const SetlistApiComponent = ({ artistName }) => {
     // This will hold the values for setlisst, which we will get from the API 
     const [artists, setArtists] = useState([]);
+    const [artistMBID, setArtistMBID] = useState("");
+    const [setlists, setSetlists] = useState([]);
+    const [sets, setSets] = useState([]);
+    const [setlistDate, setSetlistDate] = useState("");
+    const [songs, setSongs] = useState([]);
 
+    const artistMBIDMap = {};
+
+
+
+    // Get the artist data from the API based off the artist name
     useEffect(() => {
         // Create a function to fetch the data from the API
         const fetchArtistData = async () => {
@@ -24,40 +34,193 @@ const SetlistComponent = ({ artistName }) => {
                     setlistURL,
                     {
                         headers: {
-                            'x-api-key': setlistKey,
+                            'x-api-key': setlistApiKey,
                             'Accept': 'application/json'
                         }
                     }
                 )
 
+                // Set our variables to the data we get back from the API
                 setArtists(response.data.artist);
+                setSetlists([]);
+
             } catch(error) {
                 console.error('Error fetching setlist data:', error);
             };
         };
+
         fetchArtistData();
     }, [artistName]);
 
+
+    // Get setlist data from the API based off the artist MBID
+    useEffect(() => {
+        const fetchSetlistData = async () => {
+            try {
+                if (!artistMBID) {
+                    return;
+                }
+
+                const setlistURL = `/setlistapi/1.0/artist/${artistMBID}/setlists?p=1`;
+
+                const response = await axios.get(
+                    setlistURL,
+                    {
+                        headers: {
+                            'x-api-key': setlistApiKey,
+                            'Accept': 'application/json'
+                        }
+                    }
+                )
+
+                setSetlists(response.data.setlist);
+                songList = [];
+
+            } catch (error) {
+                console.error('Error fetching setlist data:', error);
+            }
+        };
+
+        fetchSetlistData();
+    }, [artistMBID]);
+
+    // Store the MBID for each artist
+    for (let i = 0; i < artists.length; i++) {
+        artistMBIDMap[artists[i].name] = artists[i].mbid;
+    }
+
+    // Store the songs from each setlist after reading the data from the API 
+    const storeSongs = () => {
+        let allSets = sets[0]['sets']['set'];
+
+        for (let set of allSets) {
+            for (let song of set['song']) {
+                // songList.push(song['name']);
+                setSongs(prevItems => [...prevItems, song['name']]);
+            }
+        }
+
+    }
+
+    // Get the songs from a setlist from the API
+    useEffect(() => {
+        const fetchSongsData = async () => {
+            try {
+                const songURL = `/setlistapi/1.0/search/setlists?artistMbid=aa7a2827-f74b-473c-bd79-03d065835cf7&date=09-12-2025&p=1`;
+                
+                const response = await axios.get(
+                    songURL,
+                    {
+                        headers: {
+                            'x-api-key': setlistApiKey,
+                            'Accept': 'application/json'
+                        }
+                    }
+                )
+
+                setSets(response.data.setlist);
+                storeSongs();
+
+            } catch (error) {
+                console.error('Error fetching songs data:', error);
+            }
+        };
+
+        fetchSongsData();
+    }, [setlistDate]);
+
+
+
+    // ================================================================================
+    // Display the data from the API 
+    // Flow: Artist -> Setlist -> Songs
+    // ================================================================================
+    
     // Only render the setlist data if we have an artist name, otherwise prompt the user to enter one
     if (!artistName) {
         return <p>Please enter an artist name to see the setlist data.</p>;
     }
 
-    return (
-        <div>
-            <h2>Artist Name: {artistName}</h2>
-            <p>Setlist data will be displayed here.</p>
-            <ul>
-                {artists.map(artist => (
-                    <li>{artist.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+    // Display the possible artists the user can pick 
+    if (artists.length > 0 && setlists.length === 0) {
+        return (
+            <div>
+                <p>Possible Artists: </p>
+                <ul>
+                    {artists.map(artist => (
+                        <li key={artist.name}>
+                            <button onClick={() => { setArtistMBID(artistMBIDMap[artist.name]) }}>{artist.name}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+    // Display the possible setlists from the artist the user picked
+    if (setlists.length > 0 && songs.length === 0) {
+        return (
+            <div>
+                <h3>Possbile Setlists:</h3>
+                <ul>
+                    {setlists.map(setlist => (
+                        <li key={setlist.id}>
+                            <button onClick={ () => setSetlistDate(setlist.eventDate) }><p>{setlist.eventDate}</p></button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+
+
+    if (songs.length > 0) {
+        return (
+            <div>
+                <h3>Setlist for {artistName} on {setlistDate}:</h3>
+                <ul>
+                    {songs.map((song, index) => (
+                        <li key={index}>{song}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+
+
+    // Display the songs from the setlist the user picked
+
+
+    // return (
+    //     <div>
+            
+    //         <h2>Artist Name: {artistName}</h2>
+
+            // <p>Possible Artists: </p>
+            // <ul>
+            //     {artists.map(artist => (
+            //         <li key={artist.name}>
+            //             <button onClick={() => { setArtistMBID(artistMBIDMap[artist.name])}}>{artist.name}</button>
+            //         </li>
+            //     ))}
+            // </ul>
+
+            // <h3>Possbile Setlists:</h3>
+            // <ul>
+            //     {setlists.map(setlist => (
+            //         <li key={setlist.id}>
+            //             <button><p>{setlist.eventDate}</p></button>
+            //         </li>
+            //     ))}
+            // </ul>
+    //     </div>
+    // );
 
 
 
 
 };
 
-export default SetlistComponent;
+export default SetlistApiComponent;
